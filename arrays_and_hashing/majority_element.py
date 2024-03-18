656ec7b9c75c938077a24b1580cfd78afd28de85
Anuraag-CH <anuraag.ch@gmail.com>
2024-03-18 15:36:43 +0530

    add code for majority-element

---
+++ arrays_and_hashing/majority_element.py
@@ -0,0 +1,18 @@
+# https://leetcode.com/problems/majority-element
+
+class Solution:
+    def majorityElement(self, nums: List[int]) -> int:
+        
+        res = 0 
+        count = 0
+
+        for n in nums :
+            if count == 0 :
+                res = n
+            count +=1 if n== res else -1
+        
+        return res
+    
+# Time complexity: O(n)
+# Space complexity: O(1)
+    
